f494f88453081d8ca8d744d8d41d2744d68cbecb
Tore Halset <halset@pvv.ntnu.no>
2017-01-10 12:35:24 +0100

    BUEP index

---
+++ src/test/java/no/jsosi/NeroyTest.java
@@ -0,0 +1,36 @@
+package no.jsosi;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import junit.framework.TestCase;
+
+public class NeroyTest extends TestCase {
+
+    public void testNeroy() throws IOException {
+        File file = new File("src/test/resources/Kartdata_1751_Neroy_UTM33_N50_SOSI.zip");
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(file));
+        ZipEntry entry = null;
+        byte[] buff = new byte[1024];
+        while ((entry = zis.getNextEntry()) != null) {
+            if (!entry.getName().endsWith(".sos")) {
+                while (zis.read(buff) > 0) {
+                }
+                continue;
+            }
+            System.out.println("XX " + entry.getName());
+            SosiReader reader = new SosiReader(zis);
+            Feature fi = null;
+            while ((fi = reader.nextFeature()) != null) {
+                assertNotNull(fi);
+                assertNotNull(fi.getGeometry());
+            }
+            reader.close();
+        }
+        zis.close();
+    }
+
+}
